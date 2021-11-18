862a57f6a99e5d7c1b1407de616195d50d1ca5e8
Sylvie-B <unsolo@hotmail.fr>
2021-11-18 11:57:54 +0100

    recup input & display text

--- exo 3/exercice.js
+++ exo 3/exercice.js
@@ -1,14 +1,13 @@
 let libButton = document.getElementById('lib-button');
+console.log(libButton);
 let libIt = function() {
+    let noun = document.getElementById('noun').value;
+    let adj = document.getElementById('adjective').value;
+    let pers = document.getElementById('person').value;
+
     let storyDiv = document.getElementById("story");
-    storyDiv.innerHTML = "??Your story here";
+    storyDiv.innerHTML = noun + " " + adj + " " + pers;
+
 };
 libButton.addEventListener('click', libIt);
-
-
-
-
-
-
-
 
